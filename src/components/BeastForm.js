96abb84e8497c77fd35f04dad0a1f0a520df3a32
Daniel Haugen <me@dnhaugen.com>
2021-04-18 16:18:20 -0700

    copmleted form filter

--- src/components/BeastForm.js
+++ src/components/BeastForm.js
@@ -2,18 +2,18 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 
 class BeastForm extends React.Component {
+
   render() {
     return(
       <Form>
         <Form.Group controlId="exampleForm.ControlSelect1">
           <Form.Label>How Many Horns:</Form.Label>
-          <Form.Control as="select">
-            <option>All</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+          <Form.Control as="select" onChange={this.props.filter}>
+            <option value='0'>All</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='100'>100</option>
           </Form.Control>
         </Form.Group>
       </Form>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,44 @@
-import HornedBeast from './HornedBeast.js';
-import React from 'react';
-import CardDeck from 'react-bootstrap/CardDeck';
-import BeastForm from './BeastForm.js'
+import HornedBeast from "./HornedBeast.js";
+import React from "react";
+import CardDeck from "react-bootstrap/CardDeck";
+import BeastForm from "./BeastForm.js";
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      likes: 0 // for example
-    }
+      hornsFilter: 0
+    };
   }
+
+  filterChangeHandler = e => this.setState({hornsFilter: parseInt(e.target.value)});
+  
 
   render() {
     return (
       <>
-        <BeastForm />
+        <BeastForm filter={this.filterChangeHandler}  />
         <CardDeck>
-          {this.props.data.map((i) => (
+          
+          {this.state.hornsFilter === 0 ? this.props.data.map((i, index) => (
             <HornedBeast
               title={i.title}
               description={i.description}
               img={i.image_url}
               horns={i.horns}
+              key={index}
             />
-          ))}
+          )) : this.props.data.filter(hornedBeast => hornedBeast.horns === this.state.hornsFilter).map((i, index) => (
+            <HornedBeast
+              title={i.title}
+              description={i.description}
+              img={i.image_url}
+              horns={i.horns}
+              key={index}
+            />
+          ))
+            };          
+          
         </CardDeck>
       </>
     );
